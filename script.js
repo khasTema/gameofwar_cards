9ba70083c67e5821ca2d50e7fa7f2ecdfb07336f
khasTema <akhasin.dev@gmail.com>
2021-11-12 13:46:48 +0200

    html and css done

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('hey');
+let deckId
+
+
+const newDeckBtn = document.getElementById('new-deck')
+const remainCards = document.getElementById('remain')
+const mainTitle = document.getElementById('main-heading')
+const drawCardsBtn = document.getElementById('draw-cards')
+
+
+newDeckBtn.addEventListener('click', gameStart)
+drawCardsBtn.addEventListener('click', gamePlay)
+
+function gameStart(){
+    newDeckBtn.textContent = "New Deck"
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+            console.log(deckId)
+            remainCards.textContent = data.remaining
+            mainTitle.textContent = 'Draw The Cards!'
+        })
+}
+
+function gamePlay(){
+    if(deckId === undefined){
+        mainTitle.textContent = "Press Start First..."
+    }else{
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+    }
+}
