14dbf99ee2f6bac0d98542f5d6ea757e1082af94
khasTema <akhasin.dev@gmail.com>
2021-11-12 17:15:44 +0200

    ver 1.0

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let deckId
 let compScore = 0
 let myScore = 0
-
-
 const newDeckBtn = document.getElementById('new-deck')
 const remainCards = document.getElementById('remain')
 const mainTitle = document.getElementById('main-heading')
@@ -10,7 +8,6 @@ const drawCardsBtn = document.getElementById('draw-cards')
 const cardsPlaceHolders = document.getElementById('cards')
 const compScoreEL = document.getElementById('comp-score')
 const myScoreEl = document.getElementById('my-score')
-
 
 newDeckBtn.addEventListener('click', gameStart)
 drawCardsBtn.addEventListener('click', gamePlay)
@@ -29,7 +26,6 @@ function gameStart(){
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
-            console.log(deckId)
             remainCards.textContent = data.remaining
             mainTitle.textContent = 'Draw The Cards!'
         })
@@ -42,8 +38,6 @@ function gamePlay(){
         fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
             .then(res => res.json())
             .then(data => {
-                console.log(data.cards)
-                console.log(cardsPlaceHolders.children )
                 for(let card of data.cards){
                     cardsPlaceHolders.children[data.cards.indexOf(card)].innerHTML=
                     `<img src="${card.image}">`
@@ -74,17 +68,12 @@ function defineTheWinner(card1, card2){
     const card1ValueIndex = valueRisingArr.indexOf(card1.value)
     const card2ValueIndex = valueRisingArr.indexOf(card2.value)
     if(card1ValueIndex === card2ValueIndex){
-        console.log('Tie')
         mainTitle.textContent = "It's a Tie!"
     }else if(card1ValueIndex > card2ValueIndex){
-        console.log('comp win')
         compScore +=1
-        // compScoreEL.textContent = compScore
         mainTitle.textContent = "Comp Wins a Score!"
     }else{
-        console.log('player win')
         myScore +=1
-        // myScoreEl.textContent = myScore
         mainTitle.textContent = "You Win a Score!"
     }
    
